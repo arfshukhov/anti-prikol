aa0763a9dc3021090165fd25487c02b7058fe144
Vladimir Shukhov <81926456+arfshukhov@users.noreply.github.com>
2022-06-14 19:09:19 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
@@ -12,8 +14,8 @@ import random
 
 import keyboard
 
+import threading
 import asyncio
-
 import os
 
 
@@ -24,15 +26,15 @@ image = ImageGrab.grab(bbox=(0, 0, SYS_W,SYS_H))
 image.save('sc.png')
 
 
-class MainLoop(QWidget):
+class Main(QWidget):
     def __init__(self):
         super().__init__()
 
-    async def init_ui(self):
+    def init_ui(self):
         self.setFixedSize(SYS_W, SYS_H)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         QShortcut(QKeySequence("Shift+Alt+Q"), self, quit)
-        keyboard.add_hotkey("alt + f4", lambda: None, suppress=True)
+        #keyboard.add_hotkey("alt + f4", lambda: None, suppress=True)
 
 
         self.show()
@@ -43,33 +45,27 @@ class MainLoop(QWidget):
         self.lab.setFont(QFont("Bold", 130))
         self.lab.show()
 
-    async def this_method_should_freeze_ur_pc(self):
-        for i in range(100):
-            await self.draw_pricall()
-        while True:
-            await self.draw_pricall()
+    def this_method_should_freeze_ur_pc(self):
+        for i in range(20):
+            self.draw_pricall()
 
-    async def draw_pricall(self):
+
+    def draw_pricall(self):
         self.label = QLabel(self)
         self.label.setStyleSheet("background-color: rgba(0, 0, 0 , 0)")
         self.label.setText("LOL LOL LOL LOL LOL")
         self.label.setGeometry(random.randrange(0, SYS_W), random.randrange(0, SYS_H), 200, 100)
         self.label.setFont(QFont("Bold", 14))
         self.label.show()
-
+        asyncio.sleep(1)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainloop = MainLoop()
+    main = Main()
 
-    async def main():
-
-        task1 = asyncio.create_task(mainloop.init_ui())
-        task2 = asyncio.create_task(mainloop.this_method_should_freeze_ur_pc())
-        await task1, task2
-    
-    asyncio.run(main())
+    threading.Thread(target=main.init_ui()).start()
+    threading.Thread(target=main.this_method_should_freeze_ur_pc()).start()
 
     os.remove("sc.png")
     app.exec()
